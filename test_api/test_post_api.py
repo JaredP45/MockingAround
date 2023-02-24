d2d7ad59610dcc61236ea4d984af71bdeb6f3d12
Jared <prokope45@pm.me>
2023-02-24 23:26:06 +0330

    Add fail unit tests

--- test_api/test_post_api.py
+++ test_api/test_post_api.py
@@ -4,17 +4,33 @@ from unittest.mock import patch
 
 # Local Import
 from main import JsonPlaceHolderThing
+from requests import HTTPError
 
 
 class TestPostAPI(TestCase):
     def test_post_api_success(self):
+        """ Test if request call is successful and if response data is as expected """
         api = JsonPlaceHolderThing()
         with patch("requests.sessions.Session.get") as mock_get_posts:
             mock_get_posts.return_value.json.return_value = self.mock_post_data()
             response = api.get_posts()
 
         mock_get_posts.assert_called()
-        print(response)
+        for post in response:
+            self.assertIn(post, self.mock_post_data())
+
+    def test_post_api_fail(self):
+        """ Test if request properly raises exception after request fail and if response is None """
+        api = JsonPlaceHolderThing()
+        error_message = str({"status": 404, "message": "Bad Request"})
+        with patch("requests.sessions.Session.get") as mock_get_posts:
+            mock_get_posts.side_effect = HTTPError(str(error_message))
+            with self.assertRaises(Exception) as exc:
+                response = api.get_posts()
+
+                self.assertIsNone(response)
+        mock_get_posts.assert_called_once()
+        self.assertIn(error_message, str(exc.exception))
 
     @staticmethod
     def mock_post_data():
